8ae5179692d0aacb37dec55fb5d5c4f92106779e
Ismat Asatov <ismatasatov@Air-Ismat.home>
2023-04-26 12:11:35 +0500

    Basic loader

--- src/views/RandomQuote.vue
+++ src/views/RandomQuote.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex gap-3.5 flex-col">
-    <QuoteCard :quote="Object.assign({}, state.quotes[state.randomIndex])" />
+    <div v-if="state.quotes.length">
+      <QuoteCard :quote="Object.assign({}, state.quotes[state.randomIndex])" />
+    </div>
+    <div v-else>Loading...</div>
+
     <div>
       <button
         class="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
